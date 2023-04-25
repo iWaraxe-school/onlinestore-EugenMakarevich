97afe9e89351b712cf849a853e23e19cfd3eb04c
EugenMakarevich <evgeny.makarevich.2019@gmail.com>
2023-04-25 22:26:13 +0200

    Creation of the domain classes

---
+++ domain/src/main/java/com/coherentsolutions/domain/categories/Category.java
@@ -0,0 +1,10 @@
+package com.coherentsolutions.domain.categories;
+
+import com.coherentsolutions.domain.products.Product;
+
+import java.util.List;
+
+public class Category {
+    private String name;
+    private List<Product> products;
+}
---
+++ domain/src/main/java/com/coherentsolutions/domain/products/Product.java
@@ -0,0 +1,7 @@
+package com.coherentsolutions.domain.products;
+
+public class Product {
+    private String name;
+    private double rate;
+    private double price;
+}
