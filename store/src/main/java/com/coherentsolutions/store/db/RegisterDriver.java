05868dea56bfbfcd3ddb006f17734fa6eba03574
EugenMakarevich <evgeny.makarevich.2019@gmail.com>
2023-06-26 20:21:40 +0200

    Fill tables category and products with data (success)

--- store/src/main/java/com/coherentsolutions/store/db/RegisterDriver.java
+++ store/src/main/java/com/coherentsolutions/store/db/RegisterDriver.java
@@ -1,7 +1,6 @@
 package com.coherentsolutions.store.db;
 
 import com.coherentsolutions.domain.categories.Categories;
-import com.coherentsolutions.domain.products.Product;
 import com.github.javafaker.Faker;
 
 import java.sql.*;
@@ -9,6 +8,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class RegisterDriver {
+
+    static Connection con;
 
     static Faker faker = new Faker();
 
@@ -18,23 +19,23 @@ public class RegisterDriver {
         Class.forName("com.mysql.cj.jdbc.Driver");
         //Getting the connection
         String mysqlUrl = "jdbc:mysql://localhost/onlinestore";
-        Connection con = DriverManager.getConnection(mysqlUrl, "root", "root");
+        con = DriverManager.getConnection(mysqlUrl, "root", "root");
         System.out.println("Connection established: " + con);
 
         Statement stmt = con.createStatement();
 
-        if (!isTableExist(con, "categories")) {
+        if (!isTableExist("categories")) {
             stmt.executeUpdate("CREATE TABLE categories (\n" +
-                    "    id int,\n" +
+                    "    id int PRIMARY KEY AUTO_INCREMENT,\n" +
                     "    category_name varchar(255)\n" +
                     ");");
         } else {
             stmt.executeUpdate("DELETE FROM categories");
         }
 
-        if (!isTableExist(con, "products")) {
+        if (!isTableExist("products")) {
             stmt.executeUpdate("CREATE TABLE products (\n" +
-                    "    id int,\n" +
+                    "    id int PRIMARY KEY AUTO_INCREMENT,\n" +
                     "    category_id int,\n" +
                     "    name varchar(255),\n" +
                     "    price decimal (10, 2),\n" +
@@ -44,12 +45,11 @@ public class RegisterDriver {
             stmt.executeUpdate("DELETE FROM products");
         }
 
-        fillStoreRandomly(con);
-        createCategories(con);
+        fillStoreRandomly();
     }
 
-    static boolean isTableExist(Connection connection, String tableName) throws SQLException {
-        PreparedStatement preparedStatement = connection.prepareStatement("SELECT count(*) "
+    static boolean isTableExist(String tableName) throws SQLException {
+        PreparedStatement preparedStatement = con.prepareStatement("SELECT count(*) "
                 + "FROM information_schema.tables "
                 + "WHERE table_name = ?"
                 + "LIMIT 1;");
@@ -60,54 +60,52 @@ public class RegisterDriver {
         return resultSet.getInt(1) != 0;
     }
 
-    public static void fillStoreRandomly(Connection con) throws SQLException {
-        createCategories(con);
+    public static void fillStoreRandomly() throws SQLException {
+        createCategories();
         int productNum = 10;
 
-        Map<Integer, String> categories = getCategories(con);
+        Map<Integer, String> categories = getCategories();
 
         for (Map.Entry<Integer, String> entry : categories.entrySet()) {
             createProducts(entry.getKey(), entry.getValue(), productNum);
         }
     }
 
-    public static void createCategories(Connection con) throws SQLException {
-        int i = 0;
+    public static void createCategories() throws SQLException {
         for (Categories eCategory : Categories.values()) {
-            i++;
-            String query = "INSERT INTO categories (id, category_name) VALUES (" + i + ",'" + eCategory.toString() + "');";
+            String query = "INSERT INTO categories (category_name) VALUES (\"" + eCategory.toString() + "\");";
             PreparedStatement ps = con.prepareStatement(query);
             ps.executeUpdate();
         }
     }
 
-    public static void createProducts(int category_id, String categoryName, int productNum) {
-        for (int i = 0; i < productNum; i++) {
-            Product product = generateProduct(i, category_id, categoryName);
-            category.addProduct(product);
+    public static void createProducts(int category_id, String categoryName, int productNum) throws SQLException {
+        for (int i = 1; i <= productNum; i++) {
+            generateProduct(category_id, categoryName);
         }
     }
 
-    public static Map<Integer, String> getCategories(Connection con) throws SQLException {
+    public static Map<Integer, String> getCategories() throws SQLException {
         String query = "select * from categories;";
         PreparedStatement ps = con.prepareStatement(query);
         ResultSet rs = ps.executeQuery(query);
 
         Map<Integer, String> categories = new HashMap<Integer, String>();
-        if (rs.next()) {
+        while (rs.next()) {
             categories.put(rs.getInt("id"), rs.getString("category_name"));
         }
         return categories;
     }
 
-    public static void generateProduct(int id, int category_id, String categoryName) {
-        String query = "INSERT INTO products (id, category_id, name, price, rate) VALUES ("
-                + id + ","
-                + category_id + ",'"
-                + generateName(categoryName) + "',"
+    public static void generateProduct(int category_id, String categoryName) throws SQLException {
+        //Do I need to create separate Category, Product class for SQL representation?
+        String query = "INSERT INTO products (category_id, name, price, rate) VALUES ("
+                + category_id + ",\""
+                + generateName(categoryName) + "\","
                 + generatePrice() + ","
                 + generateRate() + ");";
-
+        PreparedStatement ps = con.prepareStatement(query);
+        ps.executeUpdate();
     }
 
     private static String generateName(String categoryName) {
